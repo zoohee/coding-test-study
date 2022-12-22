c1c6dc0fab93709a8f05007a2b6404a2a3065a6c
zoohee <jjhh1545@gmail.com>
2022-12-23 06:02:59 +0900

    [20221223] 3 questions

---
+++ 2022.12/20221223_1.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n, x = map(int, input().split())
+
+a = list(map(int, input().split()))
+for i in range(n):
+    if a[i] < x :
+        print(a[i], end=" ")
+    
+    
---
+++ 2022.12/20221223_2.py
@@ -0,0 +1,6 @@
+#1000
+import sys
+input = sys.stdin.readline
+
+a, b = map(int,input().split())
+print(a+b)
