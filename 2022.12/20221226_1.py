a728e9ffc572eac17d4e8f7f6bab754371bcad43
zoohee <jjhh1545@gmail.com>
2022-12-27 02:48:55 +0900

    [20221226] 3 questions

---
+++ 2022.12/20221226_1.py
@@ -0,0 +1,14 @@
+#2753
+import sys
+input = sys.stdin.readline
+
+year = int(input())
+
+if year % 400 == 0 :
+    print(1)
+elif year % 100 == 0 :
+    print(1)
+elif year % 4 == 0 :
+    print(1)
+else:
+    print(0)
---
+++ 2022.12/20221226_2.py
@@ -0,0 +1,15 @@
+#2480
+import sys
+input = sys.stdin.readline
+
+a, b, c = map(int,input().split())
+
+if a == b and b == c and c == a :
+    print(10000+a*1000)
+elif a == b or b == c :
+    print(1000+b*100)
+elif c == a :
+    print(1000+c*100)
+else : 
+    print(max(a,b,c))
+    
