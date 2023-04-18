0d5f1a3b9623b05081f8e4933152b9cfc955564e
zoohee <jjhh1545@gmail.com>
2023-04-19 01:35:50 +0900

    [백트래킹] 15658 N과 M (9)

---
+++ 백트래킹/15658_N과 M (9).py
@@ -0,0 +1,28 @@
+# 시간복잡도: 
+# 최악시간: 
+# 난이도: Silver 2
+# Url: https://www.acmicpc.net/problem/15663
+# Reference: 
+import sys
+input = sys.stdin.readline
+
+s = []
+def sequence(start):
+    global visited
+    if len(s) == m:
+        print(' '.join(map(str,s)))
+        return
+    check = 0
+    for i in range(len(arr)):
+        if not visited[i] and arr[i] != check:
+            visited[i] = True
+            s.append(arr[i])
+            check = arr[i]
+            sequence(i+1)
+            visited[i] = False
+            s.pop()
+
+n, m = map(int, input().split())
+arr = sorted(list(map(int, input().split())))
+visited = [False] * n
+sequence(0)
