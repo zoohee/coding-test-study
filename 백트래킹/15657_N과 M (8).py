4eb5ddc02adc622c0c0648183fbca0e82beff65d
zoohee <jjhh1545@gmail.com>
2023-04-18 23:46:53 +0900

    [백트래킹] 15657 N과 M (8)

---
+++ 백트래킹/15657_N과 M (8).py
@@ -0,0 +1,23 @@
+# 시간복잡도: 
+# 최악시간: 
+# 난이도: Silver 3
+# Url: https://www.acmicpc.net/problem/15657
+# Reference: 
+import sys
+input = sys.stdin.readline
+
+s = []
+def sequence(start):
+    if len(s) == m:
+        print(' '.join(map(str,s)))
+        return
+    
+    for i in range(start, len(arr)):
+        s.append(arr[i])
+        sequence(i)
+        s.pop()
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+sequence(0)
