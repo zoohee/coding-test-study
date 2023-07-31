ef50ceebd1e3de664923cd7373e7baf3f130d539
zoohee <jjhh1545@gmail.com>
2023-08-01 00:14:06 +0900

    [SWEA/D3] 원재의 메모리 복구하기

---
+++ SWEA/1289_원재의 메모리 복구하기.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int cnt = 0;
+		int t = sc.nextInt();
+		sc.nextLine();
+		
+		char init = '0';
+		for (int i = 0; i < t; i++) {
+			String input = sc.nextLine();
+			init = '0';
+			cnt = 0;
+			for (int j=0; j<input.length(); j++) {
+				if (input.charAt(j) != init) {
+					init = input.charAt(j);
+					cnt++;
+				}
+			}
+			System.out.printf("#%d %d\n", i+1, cnt);
+		}
+	}
+}
