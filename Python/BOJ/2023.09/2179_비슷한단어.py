d062cbc3393ec3ef210da21b6e38cdbecea70160
zoohee <jjhh1545@gmail.com>
2023-09-13 21:26:48 +0900

    [Solving] 2179 비슷한 단어

--- Python/BOJ/2023.09/2179_비슷한단어.py
+++ Python/BOJ/2023.09/2179_비슷한단어.py
@@ -8,5 +8,105 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-word = list(input().rstrip())
-words = [input().rstrip() for _ in range(n-1)]
+input_words = [input().rstrip() for _ in range(n)]
+dict_index = dict.fromkeys(input_words, 0)
+for i in range(n):
+    dict_index[input_words[i]] = i
+words = sorted(input_words)
+print(words)
+
+maxCnt = 0
+minIndex = n
+a = ""
+b = ""
+for i in range(n-1):
+    j = i+1
+    if (words[i] == words[j]): # 같으면 건너 뛰기
+        continue
+    length = min(len(words[i]), len(words[j]))
+    cnt = 0
+    for k in range(length):
+        if(words[i][k] == words[j][k]):
+            cnt += 1
+        else:
+            break
+    
+    if (maxCnt < cnt):
+        maxCnt = cnt
+        if(dict_index[words[i]]<dict_index[words[j]]):
+            a = words[i]
+            b = words[j]
+            minIndex = dict_index[words[i]]
+        else:
+            a = words[j]
+            b = words[i]
+            minIndex = dict_index[words[j]]
+    elif (maxCnt == cnt):
+        if(minIndex > dict_index[words[i]] and
+           minIndex > dict_index[words[j]]):
+            if(dict_index[words[i]] < dict_index[words[j]]):
+                a = words[i]
+                b = words[j]
+                minIndex = dict_index[words[i]]
+            else:
+                a = words[j]
+                b = words[i]
+                minIndex = dict_index[words[j]]   
+        elif(minIndex > dict_index[words[i]]):
+            a = words[i]
+            b = input_words[minIndex]
+            minIndex = dict_index[words[i]]
+        elif(minIndex > dict_index[words[j]]):
+            a = words[j]
+            b = input_words[minIndex]
+            minIndex = dict_index[words[j]]
+        else:
+            if(minIndex == dict_index[words[i]]):
+                a = input_words[minIndex]
+                b = input_words[min(dict_index[b], dict_index[words[j]])]
+            elif(minIndex == dict_index[words[j]]):
+                a = input_words[minIndex]
+                b = input_words[min(dict_index[b], dict_index[words[i]])]
+            else:
+                a = input_words[minIndex]
+                b = input_words[min(dict_index[b], dict_index[words[i]], dict_index[words[j]])]
+                
+print(a+"\n"+b)
+# 이거 무조건 카운트 같으면 리스트에 같은거 다 추가하면서 해야됨.
+# 안그러면 쌩뚱맞게 카운트 안된 단어 들어갈 수도 있음
+# 4
+# aa
+# bb
+# bc
+# aj
+# -- 현재출력
+# aa
+# bb
+# -- 정답
+# aa
+# aj
+
+
+# 5
+# a
+# ab
+# abv
+# abaa
+# abdd
+
+# 9
+# ab
+# is
+# lunch
+# for
+# most
+# waits
+# until
+# two
+# ac
+
+# 4
+# aa
+# bb
+# bc
+# aj
