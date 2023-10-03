98784f36afd60ded1d3eddfdb67a3f21fbd318fe
zoohee <jjhh1545@gmail.com>
2023-10-03 16:46:02 +0900

    [BOJ/DP] 11404 플로이드

---
+++ Python/BOJ/2023.09/11404_플로이드.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+
+graph = [[INF]*(n+1) for _ in range(n+1)]
+for i in range(1, n+1):
+    graph[i][i] = 0
+    
+for _ in range(m):
+    s, e, w = map(int, input().split())
+    graph[s][e] = min(graph[s][e], w)
+    
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
+            
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if graph[i][j] == INF:
+            print(0,  end=" ")
+        else:
+            print(graph[i][j], end=" ")
+    print()
