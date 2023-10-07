88ff257bd00e8cefc22e02d0533d29ba17688b8e
zoohee <jjhh1545@gmail.com>
2023-10-07 12:51:50 +0900

    [SWEA/시뮬레이션] 5656 벽돌깨기

--- Python/SWEA/5656_벽돌 깨기.py
+++ Python/SWEA/5656_벽돌 깨기.py
@@ -21,73 +21,52 @@ def count_brick(graph):
 
 def drop_brick(graph):
     for j in range(w):
-        # 아래부터 올라와서 0 찾으면 swap
+        que = deque()  # 비어 있는 공간을 채울 벽돌들을 담을 스택
         for i in range(h-1, -1, -1):
-            if graph[i][j] == 0:
-                k = i
-                # while(graph[k][j] != 0 and k >= 0):
-                #     graph[k][j] = graph[k-1][j]
-                #     k -= 1
-                # if k != -1:
-                #     graph[k][j] = 0
-                break
+            if graph[i][j] != 0:
+                que.append(graph[i][j])
+                graph[i][j] = 0  # 현재 위치를 비움
+        # 스택에 담긴 벽돌들을 아래에서부터 채워넣음
+        idx = h - 1
+        while que:
+            graph[idx][j] = que.popleft()
+            idx -= 1
         
-def breaking(graph, a, b):
-    visited = [[False] * w for _ in range(h)]
-    que = deque()
-    que.append((a, b))
-    dx = [-1, 0, 1, 0]
-    dy = [0, 1, 0, -1]
+def breaking(graph, x, y):
+    # 벽돌이 부서질 위치
+    size = graph[x][y]
+    graph[x][y] = 0
 
-    while(que):
-        x, y = que.popleft()
-        for i in range(graph[x][y]):
-            for d in range(4):
-                nx = x + i*dx[d]
-                ny = y + i*dy[d]
-                if 0<=nx<h and 0<=ny<w and graph[nx][ny]!=0 and not visited[nx][ny]:
-                    graph[nx][ny] -= 1
-                    visited[nx][ny] = True
-                    if(graph[nx][ny] != 0):
-                        que.append((nx, ny))
-
-
+    # 주변 벽돌을 확인하며 부수기
+    for d in range(4):
+        for i in range(1, size):
+            nx, ny = x + dx[d] * i, y + dy[d] * i
+            if 0 <= nx < h and 0 <= ny < w and graph[nx][ny] != 0:
+                breaking(graph, nx, ny)
 
 def sol(origin_graph):
     # 중복순열
     ans = int(1e9)
     for perm in product(range(w), repeat=n):
         graph = copy.deepcopy(origin_graph)
-        for j in [2, 2, 6]: # 열
+        for j in perm: # 열
             for i in range(h): # 행
                 if graph[i][j] != 0:
                     # 벽돌 부시기
                     breaking(graph, i, j)
                     # 벽돌 떨어지기
-                    # drop_brick(graph)
+                    drop_brick(graph)
                     break
-        print_graph(graph)
+            # print_graph(graph)
         ans = min(ans, count_brick(graph))
-        break
+
     return ans
 
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
 T = int(input())
-for t in range(T):
+for t in range(1, T+1):
     n, w, h = map(int, input().split())
     origin_graph = [list(map(int, input().split())) for _ in range(h)]
-    print(sol(origin_graph))
-
-    
-
-# 1
-# 3 10 10
-# 0 0 0 0 0 0 0 0 0 0
-# 1 0 1 0 1 0 0 0 0 0
-# 1 0 3 0 1 1 0 0 0 1
-# 1 1 1 0 1 2 0 0 0 9
-# 1 1 4 0 1 1 0 0 1 1
-# 1 1 4 1 1 1 2 1 1 1
-# 1 1 5 1 1 1 1 2 1 1
-# 1 1 6 1 1 1 1 1 2 1
-# 1 1 1 1 1 1 1 1 1 5
-# 1 1 7 1 1 1 1 1 1 1
+    print("#",t," ",sol(origin_graph), sep="")
