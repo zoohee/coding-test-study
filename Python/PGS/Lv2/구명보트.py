a36c286202d3dc793c490c149ed6b4cb154b9e70
zoohee <jjhh1545@gmail.com>
2024-01-17 05:59:53 +0900

    [PGS/Lv2] 구명보트

---
+++ Python/PGS/Lv2/구명보트.py
@@ -0,0 +1,24 @@
+def solution(people, limit):
+    answer = 0
+    left = 0
+    people.sort()
+    check = False
+
+    while(people):
+        for i in range(len(people)-1, 0, -1):
+            if people[0] + people[i] <= limit:
+                answer += 1
+                people.remove(people[i])
+                people.remove(people[0])
+                check = True
+                break
+        if(check):
+            check = False
+        else:
+            answer += 1
+            people.remove(people[0])
+    
+    return answer
+
+# 정확도 테스틑 모두 통과하나, 효율성 통과 못함
+# remove() 사용하지 않고 다시 풀기
