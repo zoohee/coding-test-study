e4311981b26c6f0a1854db9b8292b1d280a1c64b
zoohee <jjhh1545@gmail.com>
2024-02-05 20:21:13 +0900

    solving

--- Python/BOJ/2024.01/2623_음악프로그램.py
+++ Python/BOJ/2024.01/2623_음악프로그램.py
@@ -7,9 +7,11 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-k = list(map(input().split()))
-numbers = []
-for i in range(1, int(k[0])):
-    numbers.append(int(k[i]))
-    
-print(numbers)
+graph = [[] for _ in range(n+1)]
+indegree = [0] * (n+1)
+
+for i in range(m):
+    arr = list(map(int, input().split()))
+    for j in range(1, arr[0]):
+        graph[arr[j]].append(arr[j+1])
+        indegree[arr[j+1]] += 1
