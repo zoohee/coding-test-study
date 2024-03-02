e3a60000e591687e1a7e619226d18e7f163d2a76
zoohee <jjhh1545@gmail.com>
2024-03-02 15:16:58 +0900

    [BOJ/S4] 1205 등수 구하기

--- Python/BOJ/2024.02/1205_등수 구하기.py
+++ Python/BOJ/2024.02/1205_등수 구하기.py
@@ -1,50 +1,23 @@
-# 실행시간: ms
-# 메모리: KB
-# 난이도: 
-# Url: https://www.acmicpc.net/problem/
-# Reference: 
+# 실행시간: 40ms
+# 메모리: 31120KB
+# 난이도: Silver 4
+# Url: https://www.acmicpc.net/problem/1205
+# Reference: me
 import sys
 input = sys.stdin.readline
 
 n, new, p = map(int, input().split())
-if n>0:
-    score = list(map(int, input().split()))
+if n==0:
+    print(1)
+    exit(0)
 else:
-    print(1)
-    exit(0)
-    
-
-# p는 랭킹리스트의 길이
-# new 가 몇등인지 구해라
-# 점수가 못들어가면 -1
-
-isInsert = False
-for i in range(len(score)-1, -1, -1):
-    if score[i] >= new and len(score) <= p:
-        if i == len(score)-1:
-            print(-1)
-            exit(0)
-        tmp = score[i+1:len(score)]
-        score = score[:i+1]
-        score.append(new)
-        isInsert = True
-        if len(score) > p:
-            score = score + tmp[:len(tmp)-1]
-        else:
-            score = score + tmp[:len(tmp)]
-        break
+    score = list(map(int, input().split()))
+    if n == p and score[-1] >= new:
+        print(-1)
     else:
-        continue
-
-if not isInsert:
-    print(1)
-    exit(0)
-
-cnt = 1
-a = score[0]
-for i in range(1, len(score)):
-    if score[i-1] != score[i]:
-        cnt += 1
-    if score[i] == new:
-        print(i+1)
-        break
+        ans = n + 1
+        for i in range(n):
+            if score[i] <= new:
+                ans = i + 1
+                break
+        print(ans)
