a175fb6172c1769f80cd4004609d48b3383b7886
JUHEE LEE <48899055+zoohee@users.noreply.github.com>
2024-03-11 12:41:14 +0900

    [BOJ/S3] 19941 햄버거 분배

--- Python/BOJ/2024.03/19941_햄버거 분배.py
+++ Python/BOJ/2024.03/19941_햄버거 분배.py
@@ -1,18 +1,30 @@
-# 실행시간: ms
-# 메모리: KB
-# 난이도: 
-# Url: https://www.acmicpc.net/problem/
-# Reference: 
+# 실행시간: 136ms
+# 메모리: 31252KB
+# 난이도: Silver 3
+# Url: https://www.acmicpc.net/problem/19941
+# Reference: me
 import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-hbg = input()
+hbg = list(input().rstrip())
 
+ans = 0
+check = False
 for i in range(len(hbg)):
-    if hbg[i]=='H':
-        print("햄버거")
-    else:
-        print("사람")
+    
+    if hbg[i] == 'P':
+        for j in range(-k, k+1):
+            if j==0:
+                continue
+            if i+j>=0 and i+j<len(hbg):
+                if hbg[i+j] == 'H':
+                    hbg[i+j] = 'A'
+                    ans += 1
+                    check = True
+                    break
+
+print(ans)
+
         
     
