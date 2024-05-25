882adbb262c4767fc54e10974bd310cd5a87ff80
zoohee <jjhh1545@gmail.com>
2024-05-25 16:09:25 +0900

    [BOJ/G5] 3020 개똥벌레

--- Python/BOJ/2024.05/3020_개똥벌레.py
+++ Python/BOJ/2024.05/3020_개똥벌레.py
@@ -1,8 +1,8 @@
-# 실행시간: ms
-# 메모리: KB
-# 난이도: 
-# Url: https://www.acmicpc.net/problem/
-# Reference: 
+# 실행시간: 236ms
+# 메모리: 59140KB
+# 난이도: Gold 5
+# Url: https://www.acmicpc.net/problem/3020
+# Reference: https://velog.io/@qazws78941/python%EB%B0%B1%EC%A4%80-3020-%ED%92%80%EC%9D%B4
 import sys
 input = sys.stdin.readline
 
@@ -10,15 +10,17 @@ n, h = map(int, input().split())
 cave = [int(input()) for _ in range(n)]
 # 아래>위>아래>위
 
-print("===============")
-# 높이만큼 시작 (아래에서 위로)
-for i in range(h):
-    count = 0
-    for j in range(n):
-        if (j+1)%2 != 0:
-            if j - cave[j] < 0:
-                count += 1
-        else:
-            if j - (h - cave[j]) > 0:
-                count += 1
-    print(count)
+lines = [0] * h
+for i in range(n):
+    if i % 2 == 0:
+        lines[h-cave[i]] += 1
+    else:
+        lines[0] += 1
+        lines[cave[i]] -= 1
+
+for i in range(1, h):
+    lines[i] += lines[i-1]
+    
+min_ans = min(lines)
+cnt = lines.count(min_ans)
+print(min_ans, cnt)
