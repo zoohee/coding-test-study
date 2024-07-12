b1de5376150ff735d711cbb1152217c61833aaf4
zoohee <jjhh1545@gmail.com>
2024-07-12 17:00:12 +0900

    [BOJ/S5] 14916 거스름돈

---
+++ Python/BOJ/2024.07/14916_거스름돈.py
@@ -0,0 +1,24 @@
+# 실행시간: ms
+# 메모리: KB
+# 난이도: 
+# Url: https://www.acmicpc.net/problem/
+# Reference: 
+
+n = int(input())
+
+ans = 0
+while True:
+    if n % 5 == 0:
+        ans += n//5
+        break
+    else:
+        n -= 2
+        ans += 1
+        
+    if n < 0:
+        break
+    
+if n < 0:
+    print(-1)
+else:
+    print(ans)
